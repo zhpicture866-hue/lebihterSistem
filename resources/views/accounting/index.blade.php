35038b752e7e94d25dad45d7f5794264ac638aae
zoelhapner <achmadzulkiflinurrochim@gmail.com>
2026-09-08 13:14:09 +0700

    update rule lagi

--- resources/views/accounting/index.blade.php
+++ resources/views/accounting/index.blade.php
@@ -8,22 +8,21 @@
                 <!-- Page title actions -->
                 <div class="col-12 col-md-auto ms-auto d-print-none">
                     <div class="btn-list">
-                @can('tambah data proyek')       
-                  <span class="d-none d-sm-inline">
-                        <a href="{{ route("accounting.create") }}" class="btn btn-dark d-none d-sm-inline-block" >
-                            <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
-                                 viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                                 stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                <line x1="12" y1="5" x2="12" y2="19"/>
-                                <line x1="5" y1="12" x2="19" y2="12"/>
-                            </svg>
-                            Tambah Akun
-                        </a>
-                    </span>
-                 @endcan
-                        
+                        @can('tambah akun-akuntansi')       
+                        <span class="d-none d-sm-inline">
+                                <a href="{{ route("accounting.create") }}" class="btn btn-dark d-none d-sm-inline-block" >
+                                    <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24"
+                                        viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
+                                        stroke-linecap="round" stroke-linejoin="round">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                                        <line x1="12" y1="5" x2="12" y2="19"/>
+                                        <line x1="5" y1="12" x2="19" y2="12"/>
+                                    </svg>
+                                    Tambah Akun
+                                </a>
+                            </span>
+                        @endcan 
                     </div>
                 </div>
             </div>
@@ -62,20 +61,44 @@
             </div>
         </div>
     </div>
+    @can('tambah akun-akuntansi')
+        <a href="{{ route('accounting.create') }}"
+        class="mobile-fab d-md-none">
+
+            <svg xmlns="http://www.w3.org/2000/svg"
+                width="26"
+                height="26"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                fill="none"
+                stroke-linecap="round"
+                stroke-linejoin="round">
+
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <line x1="12" y1="5" x2="12" y2="19"/>
+                <line x1="5" y1="12" x2="19" y2="12"/>
+
+            </svg>
+
+        </a>
+    @endcan
 @endsection
 
 @push('js')
 <script>
     $(function() {
+        const isMobile = window.innerWidth < 576;
         const table = $('#tableAccounts').DataTable({
             scrollY: '500px',
             scrollX: true,
             scrollCollapse: true,
-                fixedColumns: {
+                fixedColumns: !isMobile ? {
                     leftColumns: 3
-                },
+                } : false,
             serverSide: true,
             processing: true,
+            responsive: false,
             ajax: '{{ route("accounting.index") }}',
             columns: [
                 { data: 'DT_RowIndex', name: 'DT_RowIndex', orderable: false, searchable: false },
@@ -169,4 +192,3 @@
     });
 </script> 
 @endpush
-
